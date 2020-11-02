a3fe83ce39f6deb051e1b665e25cba067a3cdcab
mammothqt <prince_hoinach@yahoo.com>
2020-11-02 08:22:22 +0700

    Design View

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,23 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
-    include SessionsHelper
+  include SessionsHelper
 
-  	protected
+  protected
 
- 	def configure_permitted_parameters
-    sign_up_attrs = [:user_name, :email, :full_name, :password, :password_confirmation, :remember_me]
-    update_attrs = [:user_name, :email, :full_name, :password, :password_confirmation, :age, :gender]
-    devise_parameter_sanitizer.permit :sign_up, keys: sign_up_attrs
+  def configure_permitted_parameters
+    update_attrs = [:email]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+  	{ locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope "(:locale)", locale: /en|vi/ do
+    root "main_pages#home"
+    get "main_pages/home"
+    get "main_pages/help"
+    get "main_pages/about"
+    get "main_pages/contact"
+    get "/help", to: "main_pages#help"
+    get "/about", to: "main_pages#about"
+    get "/contact", to: "main_pages#contact"
+
+    devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  as :user do
-    get "sign_in" => "devise/sessions#new"
-    post "sign_in" => "devise/sessions#create"
-    delete "sign_out" => "devise/sessions#destroy"
-  end
+    as :user do
+      get "sign_in" => "devise/sessions#new"
+      post "sign_in" => "devise/sessions#create"
+      delete "sign_out" => "devise/sessions#destroy"
+    end
 
-  resources :categories do
-    resources :courses
-  end
+    resources :categories do
+      resources :courses
+    end
 
-  resources :courses do
-    resources :word_lists
-  end
+    resources :courses do
+      resources :word_lists
+    end
 
-  resources :tests do
-    resources :questions
-  end
+    resources :tests do
+      resources :questions
+    end
 
-  resources :user_results
+    resources :user_results
+  end
 end
