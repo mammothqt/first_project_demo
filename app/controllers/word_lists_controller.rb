5fcd074daafc047e3a6d390842af2144279b71a1
mammothqt <prince_hoinach@yahoo.com>
2020-11-02 10:47:12 +0700

    Add Cancancan Gem

--- app/controllers/word_lists_controller.rb
+++ app/controllers/word_lists_controller.rb
@@ -1,4 +1,5 @@
 class WordListsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_course
   before_action :set_word_list, only: [:show, :edit, :update, :destroy]
 
@@ -7,48 +8,48 @@ class WordListsController < ApplicationController
   end
 
   def index
-	@word_list = @course.word_lists.all
+	  @word_list = @course.word_lists.all
   end
 
   def show; end
 
   def create
-	@word_list = @course.word_lists.create word_list_params
+	  @word_list = @course.word_lists.create word_list_params
 
-	if @word_list.save
-	  flash[:success] = "Word has been created!"
-	  redirect_to course_path @course
-	else
-	  render new_course_word_list_path(@course)
-	end
+	  if @word_list.save
+	    flash[:success] = "Word has been created!"
+	    redirect_to course_path @course
+	  else
+	    render new_course_word_list_path(@course)
+	  end
   end
 
   def update
     if @word_list.update(word_list_params)
-	  flash[:success] = "Word has been updated"
-	  redirect_to course_path @course
-	else
-	  render edit_course_word_list_path(@course, @word_list)
-	end
+	    flash[:success] = "Word has been updated"
+	    redirect_to course_path @course
+ 	  else
+	    render edit_course_word_list_path(@course, @word_list)
+	  end
   end
 
   def destroy
-	flash[:success] = "Word has been deleted" if @word_list.destroy
-	redirect_to course_path(@course)
+	  flash[:success] = "Word has been deleted" if @word_list.destroy
+	  redirect_to course_path(@course)
   end
 
   private
   def set_course
-	@course = Course.find_by_id params[:course_id]
-	# redirect_to root_path if @category.nil?
+	  @course = Course.find_by_id params[:course_id]
+	  redirect_to root_path if @category.nil?
   end
 
   def set_word_list
-	@word_list = @course.word_lists.find_by_id params[:id]
-	# redirect_to root_path if @course.nil?
+	  @word_list = @course.word_lists.find_by_id params[:id]
+	  redirect_to root_path if @course.nil?
   end
 
   def word_list_params
-	params.require(:word_list).permit(:word, :course_id)
+	  params.require(:word_list).permit(:word, :course_id)
   end
 end
